cbd90cd2496c769546c593a43831a88c42ca56ac
stickyrst <stickyrst@gmail.com>
2018-09-15 15:22:19 +0300

    Update Tools.php

--- Tools.php
+++ Tools.php
@@ -1,7 +1,7 @@
 <?php
-/* Override - Print the RON currency always with the symbol AFTER the amount */
 class Tools extends ToolsCore
 {
+    /* Override - Print the RON currency always with the symbol AFTER the amount */
     public static function displayPrice($price, $currency = null, $no_utf8 = false, Context $context = null)
     {
         if (!is_numeric($price)) {
@@ -21,5 +21,18 @@ class Tools extends ToolsCore
         $context->language = $old_language;
 
         return $cldr->getPrice($price, is_array($currency) ? $currency['iso_code'] : $currency->iso_code);
+    }
+    
+    /* Override - Allow <a href="tel:... in HTML inputs */
+    public static function purifyHTML($html, $uri_unescape = null, $allow_style = false)
+    {
+        $hasTel = strpos($html, 'href="tel:') !== false;
+        if($hasTel) $html = str_replace('href="tel:', 'href="tel//:', $html);
+        
+        $html = parent::purifyHTML($html, $uri_unescape, $allow_style);
+        
+        // finish markup for things that HTMLPurifier doesn't support
+        if($hasTel) $html = str_replace('href="tel//:', 'href="tel:', $html);
+        return $html;
     }
 }
